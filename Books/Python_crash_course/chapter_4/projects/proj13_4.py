03ce52730ad8eb5d6c4ae1b1a27814953385591b
Murilo Rosa <murilorosa08@gmail.com>
2025-07-23 12:06:52 -0300

    commit

---
+++ Books/Python_crash_course/chapter_4/projects/proj13_4.py
@@ -0,0 +1,14 @@
+buffet_foods = ('pasta', 'pizza', 'rice and beans', 'beef','chicken ')
+
+for i in buffet_foods:
+    print(i)
+
+
+print("\n")
+
+
+
+buffet_foods = ('chocolate', 'potato', 'butter ', 'bread of cheese ','feijoada ')
+
+for i in buffet_foods:
+    print(i)
